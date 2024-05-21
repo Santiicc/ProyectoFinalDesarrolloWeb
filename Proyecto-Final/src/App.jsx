585639556250b4fad09dbbf1bdcc8bf74d7c6a79
Chorchhh <127800859+Chorchhh@users.noreply.github.com>
2024-05-21 03:21:02 -0300

    Update App.jsx

--- Proyecto-Final/src/App.jsx
+++ Proyecto-Final/src/App.jsx
@@ -1,29 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Profile from Profile;
-import Home from './Pages/Home';
-import Reviews from './Pages/Reviews';
-import NewReleases from './Pages/NewReleases';
-import Popular from './Pages/Popular';
 import './App.css'
 
-const App = () => {
   return (
-    <Router>
-      <div className="app">
-        <Profile/>
-        <div>
-          <Switch>
-            <Route path="/" exact component={Home}/>
-            <Route path="/reviews" exact component={Reviews}/>
-            <Route path="/new-releases" exact component={NewReleases}/>
-            <Route path="/popular" exact component={Popular}/>
-          </Switch>
-        </div>
-      </div>
-    </Router> 
+    <div></div>
   )
-}
+
 
 
 export default App
