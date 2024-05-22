ec56efdff1c078a7c66c4eab49bdeb9ab9c1de75
Alfonso Gesto <alfonso.gesto@correo.ucu.edu.uy>
2024-05-22 18:42:44 -0300

    Comienzos searchBar mobile

---
+++ Proyecto-Final/src/Components/searchBar.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+const SearchBar = () => {
+  const [search, setSearch] = useState('');
+  const [games, setGames] = useState([
+    'Mario Party 1',
+    'Mario Party 2',
+    'Mario Party 3',
+    'Mario Party 4',
+  ]);
+
+  const handleSearch = (e) => {
+    const search = e.target.value.toLowerCase();
+    setSearch(search);
+    const filteredGames = games.filter((game) =>
+      game.toLowerCase().includes(search));
+    setGames(filteredGames);
+  };
+  
+
+  return (
+    <div>
+      <input
+        type="search"
+        value={search}
+        onChange={handleSearch}
+        placeholder="Search game..."
+      />
+    <button onClick={handleClear}>Delete</button>
+      <ul>
+        {filteredGames.map((game) => (
+          <li key={game}>{game}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SearchBar;
