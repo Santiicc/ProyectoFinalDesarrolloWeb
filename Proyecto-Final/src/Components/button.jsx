995e06d98c9c4037b5d03e6f225518ccb4696e13
Milagros briano <milagrosbriano@gmail.com>
2024-06-14 17:32:01 -0300

    cambios finales

--- Proyecto-Final/src/Components/button.jsx
+++ Proyecto-Final/src/Components/button.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
 
-const MiBoton = ({onClick, titulo}) => {
+
+const MiBoton = ({ onClick, titulo, className, ...props }) => {
     return (
-        <button onClick={onClick}>
+        <button onClick={onClick} className={classNames(styles.button, className)} {...props}>
             {titulo}
         </button>
-    )
-}
+    );
+};
 
-export default MiBoton
+
--- Proyecto-Final/src/Pages/LoginPage.jsx
+++ Proyecto-Final/src/Pages/LoginPage.jsx
@@ -1,99 +1,235 @@
+// Importaciones de React y estilos
 import React, { useState, useEffect } from "react";
-import LoginForm from "../Components/LoginForm"; 
-import CustomH1 from "../Components/CustomH1"; 
+import CustomH1 from "../Components/CustomH1";
 import CustomP from "../Components/CustomP";
 import CustomCheckbox from "../Components/Checkbox";
-import classes from "../Pages/LoginPage.module.css"; 
+import {
+  IconEye,
+  IconRedEye,
+  IconEyeOff,
+  IconFacebook,
+  IconTwitter,
+  IconGoogle,
+} from "../Utils/Icons";
+import classes from "../Pages/LoginPage.module.css";
 
 const LoginPage = () => {
-    const [usuario, setUsuario] = useState("");
-    const [contraseña, setContraseña] = useState("");
-    const [usuarios, setUsuarios] = useState([]);
-    const [isLogin, setIsLogin] = useState(true);
-    const [rememberMe, setRememberMe] = useState(false);
+  const [usuario, setUsuario] = useState("");
+  const [contraseña, setContraseña] = useState("");
+  const [usuarios, setUsuarios] = useState([]);
+  const [isLogin, setIsLogin] = useState(true);
+  const [rememberMe, setRememberMe] = useState(false);
+  const [contraseñaVisible, setContraseñaVisible] = useState(false);
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
-    useEffect(() => {
-        const usuariosGuardados = JSON.parse(localStorage.getItem("usuarios")) || [];
-        setUsuarios(usuariosGuardados);
-    }, []);
+  useEffect(() => {
+    const usuariosGuardados =
+      JSON.parse(localStorage.getItem("usuarios")) || [];
+    setUsuarios(usuariosGuardados);
+  }, []);
 
-    const handleRegister = (e) => {
-        e.preventDefault();
-        const nuevoUsuario = { usuario, contraseña };
-        const usuariosActualizados = [...usuarios, nuevoUsuario];
-        setUsuarios(usuariosActualizados);
-        localStorage.setItem("usuarios", JSON.stringify(usuariosActualizados));
-        alert("Usuario registrado con éxito");
-        setUsuario("");
-        setContraseña("");
-        setIsLogin(true);
-    };
+  const validateEmail = (email) => {
+    return email.includes("@");
+  };
 
-    const handleLogin = (e) => {
-        e.preventDefault();
-        const usuarioEncontrado = usuarios.find(
-            (u) => u.usuario === usuario && u.contraseña === contraseña
-        );
-        if (usuarioEncontrado) {
-            alert("Login exitoso");
-        } else {
-            alert("Usuario o contraseña incorrectos");
-        }
-        setUsuario("");
-        setContraseña("");
-    };
+  const handleRegister = (e) => {
+    e.preventDefault();
+    setEmailError("");
+    setPasswordError("");
 
-    const handleRememberMe = (e) => {
-        setRememberMe(e.target.checked);
-    };
+    if (!validateEmail(usuario)) {
+      setEmailError("Invalid email");
+    }
 
-    return (
-        <div className={classes.container}>
-            <CustomH1 fontFamily="Exo" fontSize="20px" fontWeight="700"> Welcome! Log in or register</CustomH1>
-            <CustomP fontFamily="Exo" fontSize="16px" fontWeight="400">Log in to find the games you're looking for!</CustomP>
-            <form onSubmit={isLogin ? handleLogin : handleRegister}>
-                <div className={classes.inputBlock}>
-                    <input 
-                        type="text" 
-                        name="usuario" 
-                        value={usuario} 
-                        onChange={(e) => setUsuario(e.target.value)} 
-                        placeholder="Email"
-                    />
-                </div>
-                <div className={classes.inputBlock}>
-                    <input 
-                        type="password" 
-                        name="contraseña" 
-                        value={contraseña} 
-                        onChange={(e) => setContraseña(e.target.value)} 
-                        placeholder="Password"
-                    />
-                </div>
-                <div className={classes.checkboxContainer}>
-                    <CustomCheckbox checked={rememberMe} onChange={handleRememberMe} label="Remember me" />
-                    <CustomP fontFamily="Open Sans" fontSize="14px" fontWeight="400">
-                        <a href="#" style={{ color: '#5FE19B', textDecoration: 'none' }}>Forgot password?</a>
-                    </CustomP>
-                </div>
-                <button type="submit" className="custom-button" style={{ fontFamily: 'Exo', fontSize: '18px', fontWeight: '700' }}>
-                    {isLogin ? "Log in" : "Register"}
-                </button>
-            </form>
-            <button className="switch" onClick={() => setIsLogin(!isLogin)}>
-                {isLogin ? 
-                    <CustomP fontFamily="Open Sans" fontSize="14px" fontWeight="400">Not registered yet? <span style={{ color: '#5FE19B' }}>Register Now</span></CustomP> :
-                    <CustomP fontFamily="Open Sans" fontSize="14px" fontWeight="400">Already registered? Log in</CustomP>
-                }
-            </button>
+    if (!contraseña) {
+      setPasswordError("Invalid email or password");
+      setContraseñaVisible(true);
+    }
 
-            <div className="social-login-buttons">
-                <button className="facebook">Login with Facebook</button>
-                <button className="twitter">Login with Twitter</button>
-                <button className="google">Login with Google</button>
+    if (validateEmail(usuario) && contraseña) {
+      const nuevoUsuario = { usuario, contraseña };
+      const usuariosActualizados = [...usuarios, nuevoUsuario];
+      setUsuarios(usuariosActualizados);
+      localStorage.setItem("usuarios", JSON.stringify(usuariosActualizados));
+      alert("Usuario registrado con éxito");
+      setUsuario("");
+      setContraseña("");
+      setIsLogin(true);
+    }
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setEmailError("");
+    setPasswordError("");
+
+    if (!validateEmail(usuario)) {
+      setEmailError("Invalid email");
+    }
+
+    if (!contraseña) {
+      setPasswordError("Invalid email or password");
+      setContraseñaVisible(true);
+    }
+
+    if (validateEmail(usuario) && contraseña) {
+      const usuarioEncontrado = usuarios.find(
+        (u) => u.usuario === usuario && u.contraseña === contraseña
+      );
+      if (usuarioEncontrado) {
+        alert("Login exitoso");
+      } else {
+        setPasswordError("Invalid email or password");
+        setContraseñaVisible(true);
+      }
+    }
+    setUsuario("");
+    setContraseña("");
+  };
+
+  const handleRememberMe = (e) => {
+    setRememberMe(e.target.checked);
+  };
+
+  return (
+    <div className={classes.container}>
+      <div className="conteiner-login">
+        <CustomH1 fontFamily="Exo" fontSize="20px" fontWeight="700">
+          Welcome! Log in or register
+        </CustomH1>
+        <CustomP fontFamily="Exo" fontSize="16px" fontWeight="400">
+          Log in to find the games you're looking for!
+        </CustomP>
+        <form onSubmit={isLogin ? handleLogin : handleRegister}>
+          <div className={`${classes.inputBlock} ${emailError || passwordError ? classes.error : ''}`}>
+            <input
+              type="text"
+              name="usuario"
+              value={usuario}
+              onChange={(e) => setUsuario(e.target.value)}
+              placeholder="Email"
+              className={classes.inputField}
+            />
+            {emailError && <p className={classes.errorMessage}>{emailError}</p>}
+          </div>
+          <div className={`${classes.inputBlock} ${passwordError ? classes.error : ''}`}>
+            <div className={classes.passwordContainer}>
+              <input
+                type={contraseñaVisible ? "text" : "password"}
+                name="contraseña"
+                value={contraseña}
+                onChange={(e) => setContraseña(e.target.value)}
+                placeholder="Password"
+                className={`${classes.inputField}`}
+              />
+              <button
+                type="button"
+                className={classes.eyeButton}
+                onClick={() => setContraseñaVisible(!contraseñaVisible)}
+              >
+                {passwordError ? <IconRedEye /> : contraseñaVisible ? <IconEye /> : <IconEyeOff />}
+              </button>
             </div>
+            {passwordError && <p className={classes.errorMessage}>{passwordError}</p>}
+          </div>
+          <div className={classes.checkboxContainer}>
+            <CustomCheckbox
+              checked={rememberMe}
+              onChange={handleRememberMe}
+              label="Remember me"
+            />
+            <CustomP
+              fontFamily="Open Sans"
+              fontSize="14px"
+              fontWeight="400"
+            >
+              <a
+                href="#"
+                style={{ color: "#5FE19B", textDecoration: "none" }}
+              >
+                Forgot password?
+              </a>
+            </CustomP>
+          </div>
+          <button
+            type="submit"
+            className={classes.customButton} 
+            style={{
+              fontFamily: "Exo",
+              fontSize: "18px",
+              fontWeight: "700",
+            }}
+          >
+            {isLogin ? "Log in" : "Register"}
+          </button>
+        </form>
+        <button className={classes.switchButton} onClick={() => setIsLogin(!isLogin)}>
+          {isLogin ? (
+            <CustomP fontFamily="Open Sans" fontSize="14px" fontWeight="400">
+              Not registered yet?{" "}
+              <span style={{ color: "#5FE19B" }}>Register Now</span>
+            </CustomP>
+          ) : (
+            <CustomP fontFamily="Open Sans" fontSize="14px" fontWeight="400">
+              Already registered? Log in
+            </CustomP>
+          )}
+        </button>
+        <div className={classes.lineContainer}>
+          <div className={classes.line}></div>
+          <CustomP
+            margin="20px 0;"
+            fontFamily="Open Sans"
+            fontSize="14px"
+            fontWeight="400"
+            className={classes.or}
+          >
+            or
+          </CustomP>
+          <div className={classes.line}></div>
         </div>
-    );
+        <div className={classes.socialButtons}>
+          <button className={classes.facebook}>
+            <div className={classes.icon}>
+              <IconFacebook />
+            </div>
+            <CustomP
+              fontFamily="Exo"
+              fontWeight="700"
+              fontSize="18px"
+            >
+              Login with Facebook
+            </CustomP>
+          </button>
+          <button className={classes.twitter}>
+            <div className={classes.icon}>
+              <IconTwitter />
+            </div>
+            <CustomP
+              fontFamily="Exo"
+              fontWeight="700"
+              fontSize="18px"
+            >
+              Login with Twitter
+            </CustomP>
+          </button>
+          <button className={classes.google}>
+            <div className={classes.icon}>
+              <IconGoogle />
+            </div>
+            <CustomP
+              fontFamily="Exo"
+              fontWeight="700"
+              fontSize="18px"
+            >
+              Login with Google
+            </CustomP>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
