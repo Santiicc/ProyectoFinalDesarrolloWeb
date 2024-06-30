6a0f7d0338b78c170b6b698896f6957aba1691f7
unknown <jorge.mendez@correo.ucu.edu.uy>
2024-06-29 23:09:02 -0300

    Algunos Cambios

--- Proyecto-Final/src/App.jsx
+++ Proyecto-Final/src/App.jsx
@@ -1,11 +1,6 @@
 import "./App.css";
 import Sidebar from "./Components/Sidebar/sidebar";
-import Home from "./Pages/Home";
-import Reviews from "./Pages/Reviews";
-import NewReleases from "./Pages/NewReleases";
-import Popular from "./Pages/Popular";
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
   const [themeSettings, setThemeSettings] = useState({
@@ -20,17 +15,7 @@ export default function App() {
 
   return (
       <div className={"App-" + themeSettings.mode}>
-        <Router>
           <Sidebar />
-          <main>
-            <Routes>
-              <Route pathname="/" element={<Home />} />
-              <Route pathname="/reviews" element={<Reviews />} />
-              <Route pathname="/newreleases" element={<NewReleases />} />
-              <Route pathname="/popular" element={<Popular />} />
-            </Routes>
-          </main>
-        </Router>
       </div>
   );
 }
--- Proyecto-Final/src/Components/Sidebar/sidebar.jsx
+++ Proyecto-Final/src/Components/Sidebar/sidebar.jsx
@@ -1,85 +1,45 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import styles from "./../../Styles/sidebar.module.css";
-import estrella from "./../../assets/icons/estrella.svg";
-import calendario from "./../../assets/icons/calendario.svg";
-import reloj from "./../../assets/icons/reloj.svg";
-import lupita from "./../../assets/icons/lupita.svg"
-import best from "./../../assets/icons/best.svg"
+import styles from "./../../Styles/sidebar.module.css"
+import estrella from "../../assets/icons/estrella.svg";
+import calendario from "../../assets/icons/calendario.svg";
+import reloj from "../../assets/icons/reloj.svg";
+import lupita from "../../assets/icons/lupita.svg";
+import best from "../../assets/icons/best.svg";
 
 const Sidebar = () => {
   return (
-    <div>
-      <div className={styles.topBox}>
-        <p>GameFinder</p>
+    <div className={styles.sidebar}>
+      <div className="container1">
+        <h5 className={styles.title}>Home</h5>
+        <h5 className={styles.titles}>Reviews</h5>
+        <h5 className={styles.titles}>New Releases</h5>
+        <div className={styles.iconTextContainer}>
+          <img src={estrella} className={styles.icon} alt="Icono de estrella" />
+          <h6 className={styles.miniTitles}>This Week</h6>
+        </div>
+        <div className={styles.iconTextContainer}>
+          <img src={calendario} className={styles.icon} alt="Icono de calendario" />
+          <h6 className={styles.miniTitles}>This Month</h6>
+        </div>
+        <div className={styles.iconTextContainer}>
+          <img src={reloj} className={styles.icon} alt="Icono de reloj" />
+          <h6 className={styles.miniTitles}>Coming Soon</h6>
+        </div>
       </div>
-      <div>
-        <h1 className={styles.top}>New and trending</h1>
-        <p className={styles.top2}>Based on player counts and release date</p>
-      </div>
-      <div className={styles.sidebar}>
-        <nav>
-          <ul className={styles.navList}>
-            <li className={styles.navItem}>
-              <Link to="/" className={styles.navLink}>Home</Link>
-            </li>
-            <li className={styles.navItem}>
-              <Link to="/reviews" className={styles.navLink}>Reviews</Link>
-            </li>
-            <li className={styles.navItem}>
-              <Link to="/new-releases" className={styles.navLink}>New Releases</Link>
-              <ul className={styles.navList2}>
-                <li className={styles.navItem2}>
-                  <Link to="/new-releases/thisWeek" className={styles.navLink2}>
-                    <div className={styles.iconTextContainer}>
-                      <img src={estrella} className={styles.icon} alt="estrella" />
-                      <span>This week</span>
-                    </div>
-                  </Link>
-                </li>
-                <li className={styles.navItem2}>
-                  <Link to="/new-releases/thisWeek" className={styles.navLink2}>
-                    <div className={styles.iconTextContainer}>
-                      <img src={calendario} className={styles.icon} alt="calendario" />
-                      <span>This month</span>
-                    </div>
-                  </Link>
-                </li>
-                <li className={styles.navItem2}>
-                  <Link to="/new-releases/thisWeek" className={styles.navLink2}>
-                    <div className={styles.iconTextContainer}>
-                      <img src={reloj} className={styles.icon} alt="reloj" />
-                      <span>Coming soon</span>
-                    </div>
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li className={styles.navItem}>
-              <Link to="/popular" className={styles.navLink}>Popular</Link>
-              <ul className={styles.navList2}>
-                <li className={styles.navItem2}>
-                  <div className={styles.iconTextContainer}>
-                    <img src={lupita} className={styles.icon} alt="lupita" />
-                    <span>Coming soon</span>
-                  </div>
-                </li>
-                <li className={styles.navItem2}>
-                  <div className={styles.iconTextContainer}>
-                    <img src={best} className={styles.icon} alt="best" />
-                    <span>Best Of The Year</span>
-                  </div>
-                </li>
-                <li className={styles.navItem}>
-                  <Link to="/sidebar-mobile" className={styles.navLink}>Sidebar Mobile</Link>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </nav>
+      <div className="container2">
+        <h5 className={styles.titles}>Popular</h5>
+        <div className={styles.iconTextContainer}>
+          <img src={lupita} className={styles.icon} alt="Icono de lupita" />
+          <h6 className={styles.miniTitles}>Last Searches</h6>
+        </div>
+        <div className={styles.iconTextContainer}>
+          <img src={best} className={styles.icon} alt="Icono de best" />
+          <h6 className={styles.miniTitles}>Best of the Year</h6>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Sidebar;
+
